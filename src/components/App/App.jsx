64a47bb0f8ee1eff9939acb034bf231106774573
DivideBySara <email address>
2019-06-02 23:47:38 -0700

    initial commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import Calculator from '../Calculator/Calculator';
-import './App.css';
 
 const App = () => (
   <div className="app-container">
-    <Calculator />
+    <p>Hello, calculator!</p>
   </div>
 );
 
